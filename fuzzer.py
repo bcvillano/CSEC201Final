4b4676f9a0297d12de19d8979636dacf0b5628c8
bvillano05 <bvillano05@gmail.com>
2024-12-12 12:17:30 -0500

    Update fuzzer + exploit

--- fuzzer.py
+++ fuzzer.py
@@ -2,19 +2,23 @@
 
 import socket,time
 
-IP = ""
-PORT = 1234
+IP = "192.168.192.167"
+PORT = 2223
 
-count = 0
+count = 1
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect((IP, PORT))
 
+print("Fuzzing the server...")
 while True:
-    print("Iteration: ", count)
-    sock.send("A".encode() * count)
+    print("\nIteration: ", count)
+    sock.send("1".encode() * count)
     response = sock.recv(1024)
+    print(response)
+    count += 1
     if response:
         time.sleep(0.05)
         continue
     else:
         break
+print("Fuzzing crashed at iteration", count)
